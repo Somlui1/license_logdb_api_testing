b1e89f2beaf154bc5c8c8cdc63b431c0db42d2ae
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-08-28 13:30:09 +0700

    update testing path

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 # Dynamic payload handler
 @app.post("/testing/{parameter}")
-async def get_payload_dynamic(parameter : str,payload: Any = Body(...)):
+async def get_payload_dynamic(parameter : int,payload: Any = Body(...)):
     json_payload = jsonable_encoder(payload)
     return {"received_payload": json_payload,
             "parameter" : parameter 
