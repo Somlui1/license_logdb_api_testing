72540d7b06b12882db309ef88729f4c65f6f0183
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-08-28 15:03:35 +0700

    update testing path

--- app/main.py
+++ app/main.py
@@ -1,30 +1,22 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import Any
-from sqlalchemy import create_engine, text
 from fastapi.encoders import jsonable_encoder
-from datetime import date, time
-from decimal import Decimal
-from typing import Optional
+from typing import List
+
 app = FastAPI()
 
-class NXSessionCreate(BaseModel):
-    start_date: Optional[date]
-    start_time: Optional[time]
-    end_date: Optional[date]
-    end_time: Optional[time]
-    duration_minutes: Optional[Decimal]
-    hostname: Optional[str]
-    module: Optional[str]
-    username: Optional[str]
+class data(BaseModel):
+    username: str
+    email: str
+
+class license_input(BaseModel):
+    ip : int
+    hostname: str
+    data : List[data] 
 
 # Dynamic payload handler
-@app.post("/testing/{parameter}")
-async def get_payload_dynamic(parameter: str, payload: Any = Body(...)):
-    # แปลง payload เป็น dict
-    json_payload = jsonable_encoder(payload)
-
-    # ดึงค่าจาก dict ตาม parameter
-    value = json_payload.get(parameter, None)
-    
-    return {"message": "Session received", "field": parameter, "value": value}
+@app.post("/testing/")
+async def get_payload_dynamic(payload : license_input):
+    payload = jsonable_encoder(payload)
+    return payload
