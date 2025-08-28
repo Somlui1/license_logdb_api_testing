934ab1a978cb186887ea30621017ac3c8841d199
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-08-28 13:24:58 +0700

    edit requiment.txt

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import Any
 from sqlalchemy import create_engine, text
-
+from fastapi.encoders import jsonable_encoder
 app = FastAPI()
 
 # Dynamic payload handler
 @app.post("/testing/")
 async def get_payload_dynamic(payload: Any = Body(...)):
-    return {"received_payload": payload}
+    json_payload = jsonable_encoder(payload)
+    return {"received_payload": json_payload}
