0006cef4fe689c6d9878d178daeae76dc8b5ea40
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-08-29 15:52:13 +0700

    update testing path

--- app/db.py
+++ app/db.py
@@ -1,52 +1,63 @@
-from sqlalchemy import Column, Integer, String, DateTime, Date, Time, Numeric
+from sqlalchemy import Column, Integer, String, DateTime, Date, Time, Numeric, create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
-from sqlalchemy.dialects.postgresql import JSONB
 
 Base = declarative_base()
-# Dynamic schemas
+schemas = ["autoform", "nx", "catia", "solidworks", "autodesk", "testing"]
 
-# ตัวอย่าง session_logs สำหรับ software ต่างๆ
-class NXSession(Base):
-    __tablename__ = "session_logs"
-    __table_args__ = {"schema": "nx"}
-    id = Column(Integer, primary_key=True)
-    start_date = Column(Date)
-    start_time = Column(Time)
-    end_date = Column(Date)
-    end_time = Column(Time)
-    duration_minutes = Column(Numeric(10,2))
-    hostname = Column(String)
-    module = Column(String)
-    username = Column(String)
+def greet(sqlalchemy_engine_url):
+    engine = create_engine(sqlalchemy_engine_url)
 
-class AutoformSession(Base):
-    __tablename__ = "session_logs"
-    __table_args__ = {"schema": "autoform"}
-    id = Column(Integer, primary_key=True)
-    start_date = Column(Date)
-    start_time = Column(Time)
-    start_hours = Column(Integer)
-    start_action = Column(String)
-    end_date = Column(Date)
-    end_time = Column(Time)
-    end_hours = Column(Integer)
-    end_action = Column(String)
-    duration_minutes = Column(Numeric(10,2))
-    host = Column(String)
-    module = Column(String)
-    username = Column(String)
-    version = Column(String)
+    # สร้าง schemas
+    with engine.begin() as conn:
+        for schema_name in schemas:
+            conn.execute(text(f'CREATE SCHEMA IF NOT EXISTS "{schema_name}"'))
 
-class SolidworkSession(Base):
-    __tablename__ = "session_logs"
-    __table_args__ = {"schema": "solidworks"}
-    id = Column(Integer, primary_key=True)
-    start_date = Column(Date)
-    start_time = Column(Time)
-    end_date = Column(Date)
-    end_time = Column(Time)
-    duration_minutes = Column(Numeric(10,2))
-    feature = Column(String)
-    username = Column(String)
-    computer = Column(String)
+    #====================================
+    # Define tables
+
+    class Nx(Base):
+        __tablename__ = "session_logs"
+        __table_args__ = {"schema": "nx"}
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        start_date = Column(Date)
+        start_time = Column(Time)
+        end_date = Column(Date)
+        end_time = Column(Time)
+        duration_minutes = Column(Numeric(10,2))
+        hostname = Column(String)
+        module = Column(String)
+        username = Column(String)
+
+    class Autoform(Base):
+        __tablename__ = "session_logs"
+        __table_args__ = {"schema": "autoform"}
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        start_date = Column(Date)
+        start_time = Column(Time)
+        start_hours = Column(Integer)
+        start_action = Column(String)
+        end_date = Column(Date)
+        end_time = Column(Time)
+        end_hours = Column(Integer)
+        end_action = Column(String)
+        duration_minutes = Column(Numeric(10,2))
+        host = Column(String)
+        module = Column(String)
+        username = Column(String)
+        version = Column(String)
+
+    class Solidworks(Base):
+        __tablename__ = "session_logs"
+        __table_args__ = {"schema": "solidworks"}
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        start_date = Column(Date)
+        start_time = Column(Time)
+        end_date = Column(Date)
+        end_time = Column(Time)
+        duration_minutes = Column(Numeric(10,2))
+        feature = Column(String)
+        username = Column(String)
+        computer = Column(String)
+
+    # สร้างทุก table
+    Base.metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Any, List
 from app import valid # Import models from valid.py
-app = FastAPI()
+from app import db
+from sqlalchemy import Column, Integer, String, Date, Time, Numeric, DateTime, create_engine, text
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+from faker import Faker
+from sqlalchemy import create_engine       # สร้าง connection engine
+from sqlalchemy.orm import sessionmaker    # สร้าง session สำหรับ insert/query
 
+#ORM setup
+engine_url = "postgresql://itsupport:aapico@10.10.3.215:5432/license_logsdb"
+engine = create_engine(engine_url)
+Base = declarative_base()
+Base.metadata.create_all(engine)
+
+#FastAPI app
+app = FastAPI()
 class LicenseInput(BaseModel):
     ip: int
     product: str   # ใช้เป็น key เลือก model
@@ -15,15 +30,15 @@ async def get_payload_dynamic(payload: LicenseInput):
     cls = getattr(valid, payload.product, None)
     #cls = None
     if not cls:
-        return {"error": f"Model '{payload.hostname}' not found"}
+        return {"error": f"Model '{payload.product}' not found"}
 
     try:
         parsed_data = [cls(**item) for item in payload.data]
+    
     except Exception as e:
         return {"error": str(e)}
-
     return {
         "ip": payload.ip,
-        "product": payload.hostname,
+        "product": payload.product,
         "parsed_data": [d.dict() for d in parsed_data]
     }
