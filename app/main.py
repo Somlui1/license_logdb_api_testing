7dc75d8b944327e7334d6046bef1e8a1df847f2d
Wajeepradit Prompan (AH) <wajeepradit.p@aapico.com>
2025-10-08 12:01:14 +0700

    .

--- app/main.py
+++ app/main.py
@@ -75,6 +75,15 @@ async def get_payload_dynamic(payload: license_log_validate.LicenseInput):
             session.add(log_entry)
             session.commit()
 
+        if payload.raw:
+            RawLogs = license_logsdb.raw_logs_table(schema_name=payload.product,table_name='raw_logs')
+            log_entry = RawLogs.from_pydantic(payload.row, batch_id=share_uuid)
+            with Session_license_logsdb() as session:
+                session.add(log_entry)
+                session.commit()
+
+
+
     except Exception as e:
         return {"error": str(e)}
 
